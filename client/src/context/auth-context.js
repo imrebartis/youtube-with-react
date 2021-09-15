440d304b6bb0f450ca8ecceb6809831ab694c525
imrebartis <imbartis@gmail.com>
2021-09-15 15:01:41 +0300

    fetch data performantly with react-query

--- client/src/context/auth-context.js
+++ client/src/context/auth-context.js
@@ -1,14 +1,22 @@
 import React from "react";
+import { useQuery } from "react-query";
 import { client } from "../utils/api-client";
 
 const AuthContext = React.createContext(null);
 
 export function AuthProvider({ children }) {
-  const [user, setUser] = React.useState(null);
+  const { data } = useQuery("AuthProvider", () =>
+    client.get("auth/me").then((res) => res.data.user)
+  );
 
-  React.useEffect(() => {
-    client.get("auth/me").then((res) => setUser(res.data.user));
-  }, []);
+  const user = data || null;
+
+  console.log(data);
+  // const [user, setUser] = React.useState(null);
+
+  // React.useEffect(() => {
+  //   client.get("auth/me").then((res) => setUser(res.data.user));
+  // }, []);
 
   return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
 }
