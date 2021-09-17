b06c02f2c35d9ff137708b05818873ddca7c15eb
imrebartis <imbartis@gmail.com>
2021-09-17 16:05:51 +0300

    get recommended videos in home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,25 @@
 import React from "react";
 import Wrapper from "../styles/Home";
 import VideoGrid from "../styles/VideoGrid";
+import { useQuery } from "react-query";
+import { client } from "../utils/api-client";
+import HomeSkeleton from "../skeletons/HomeSkeleton";
+import ErrorMessage from "../components/ErrorMessage";
 
 function Home() {
+  const {
+    data: videos,
+    isLoading,
+    isError,
+    error,
+  } = useQuery("Home", () =>
+    client.get("/videos").then((res) => res.data.videos)
+  );
+
+  console.log(videos);
+
+  if (isLoading) return <HomeSkeleton />;
+  if (isError) return <ErrorMessage error={error} />;
   return (
     <Wrapper>
       <VideoGrid>Recommended videos</VideoGrid>
